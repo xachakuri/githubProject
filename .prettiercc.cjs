f6a0ca2ab7e5124a844779abc31613cd267e5805
Artem Kurov <kur0v-artem@yandex.ru>
2023-05-07 11:03:33 +0300

    init project

---
+++ .prettiercc.cjs
@@ -0,0 +1,9 @@
+module.exports = {
+    bracketSpacing: true,
+    singleAttributePerLine: true,
+    singleQuote: true,
+    trailingComma: 'es5',
+    tabWidth: 2,
+    semi: true,
+    arrowParens: 'avoid',
+};
---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <p>fdsaf sad 123</p>
+    </div>
+  );
+}
+
+export default App;
